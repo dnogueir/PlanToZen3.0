974682372a23755a501508a88dc06f84ec00b113
dnogueir <dnogueir@users.noreply.github.com>
2017-10-12 15:02:49 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/aluno/plantozen20/MainActivity.java
+++ app/src/main/java/com/example/aluno/plantozen20/MainActivity.java
@@ -1,8 +1,7 @@
-package com.example.aluno.plantozen20;
+package com.example.aluno.plantozen20.activity;
 
+import android.content.Context;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -17,7 +16,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.aluno.plantozen20.model.Anotacoes;
+import com.example.aluno.plantozen20.model.Atividades;
+import com.example.aluno.plantozen20.model.AtividadesTMI;
+
+
 import android.widget.TextView;
+
+import com.example.aluno.plantozen20.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), MainActivity.this);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
@@ -54,16 +60,19 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.menu);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        // Iterate over all tabs and set the custom view
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            tab.setCustomView(mSectionsPagerAdapter.getItem(i).getView());
+        }
 
             //TESTE COMENTÁRIO NA BRANCH
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
 
@@ -119,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+         //   textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
@@ -128,18 +137,24 @@ public class MainActivity extends AppCompatActivity {
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
+
+
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        String tabTitles[] = new String[] { "TMI's", "Tarefas", "Anotações" };
+        Context context;
+
+        public SectionsPagerAdapter(FragmentManager fm, Context context) {
             super(fm);
+            this.context = context;
         }
 
-        @Override
-        public Fragment getItem(int position) {
+      //  @Override
+     //   public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
+          //  return PlaceholderFragment.newInstance(position + 1);
+      //  }
 
         @Override
         public int getCount() {
@@ -148,16 +163,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public CharSequence getPageTitle(int position) {
+        public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return "TMI's";
+                    return new AtividadesTMI();
                 case 1:
-                    return "Tarefas";
+                    return new Atividades();
                 case 2:
-                    return "Anotações";
+                    return new Anotacoes();
             }
             return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // Generate title based on item position
+            return tabTitles[position];
         }
     }
 }
