22fb991c4c60af64a94ffca01e3b2b76411abfb7
tamiris-fonseca <tamiris.fonesca@unifei.edu.br>
2017-10-19 20:07:19 -0200

    Novos arquivos

---
+++ app/src/main/java/com/example/aluno/plantozen20/newAnotation.java
@@ -0,0 +1,36 @@
+package com.example.aluno.plantozen20;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.content.DialogInterface;
+
+/**
+ * Created by tamir on 01/10/2017.
+ */
+
+public class newAnotation extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.nova_anotacao, null))
+               .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+               .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+               })
+               .setTitle("Nova Anotação");
+
+        return builder.create();
+    }
+}
