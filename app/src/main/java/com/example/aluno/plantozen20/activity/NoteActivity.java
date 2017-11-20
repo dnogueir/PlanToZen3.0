e19e34c27b5d140d65258dda317eb323f886b9c2
dnogueir <daniell_dng@hotmail.com>
2017-11-20 09:16:33 -0200

    botão de cancelar funcionando

--- app/src/main/java/com/example/aluno/plantozen20/activity/NoteActivity.java
+++ app/src/main/java/com/example/aluno/plantozen20/activity/NoteActivity.java
@@ -1,11 +1,18 @@
 package com.example.aluno.plantozen20.activity;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.aluno.plantozen20.R;
+import com.example.aluno.plantozen20.adapter.newAnotation;
 
 /**
  * Created by Danie on 07/11/2017.
@@ -32,6 +39,13 @@ public class NoteActivity extends AppCompatActivity {
 
         // Set up the ViewPager with the sections adapter.
         // mViewPager = (ViewPager) findViewById(R.id.container);
+
+        Button Cancelar = (Button) findViewById(R.id.buttonCancelar);
+        setOnClickCancelar(Cancelar, this);
+
+        Button Salvar = (Button) findViewById(R.id.buttonSalvar);
+        setOnClickSalvar(Salvar, this);
+
         TextView texto = (TextView)findViewById(R.id.editText_nomenota);
 
         Bundle b = getIntent().getExtras();
@@ -40,8 +54,29 @@ public class NoteActivity extends AppCompatActivity {
         //  System.out.println(value);
         texto.setText(titulo);
         //Log.d(""+value);
+        //AQUI DEVE PROCURAR NO BANCO O CARD CLICADO E MOSTRAR NA TEXTBOX AS ANOTAÇÕES SALVAS
 
 
+    }
+
+    private void setOnClickCancelar(Button cancelar, final NoteActivity mainActivity){
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+//                Intent intent = new Intent(NoteActivity.this, MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
+                finish();
+
+            }
+        });
+    }
+
+    private void setOnClickSalvar(Button salvar, final NoteActivity mainActivity){
+        salvar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+               //SALVAR NO BANCO DE DADOS AS ANOTAÇÕES ESCRITAS
+            }
+        });
     }
 
     @Override
